3374b26d9513590709f118a356f4fbd9fb1ba3e1
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2023-12-17 09:08:29 +0330

    Add navbar

---
+++ src/assets/pages/Navbar1.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navbar1 = () => {
+  return (
+    <div>
+<img src=" https://coursee.org/utils/logo.svg" alt=""  className=' w-129px h-46px  rounded-none '/>
+
+<span className="min-w-120px cursor-pointer !hover:text-blue rtl text-xs text-gray font-600 text-14px">إنشاء المحتوى</span>
+
+
+<div>
+
+
+<input type=' text'  placeholder=' ماذا تريد ان تتعلم اليوم '/>
+ 
+</div>
+    </div>
+  )
+}
+
+export default Navbar1
