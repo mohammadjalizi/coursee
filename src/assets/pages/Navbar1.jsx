1e38f4c9192af7496b822aeb5817657862a4b8b8
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2023-12-17 11:07:51 +0330

    end navbar

--- src/assets/pages/Navbar1.jsx
+++ src/assets/pages/Navbar1.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
-
+import { CiSearch } from "react-icons/ci";
 const Navbar1 = () => {
     return (
-        <div className='   mx-6 flex items-center justify-between'>
+        <div className='      px-36  flex items-center justify-between'>
            <div className=' flex  items-center  gap-8 '> 
             
             <img src=" https://coursee.org/utils/logo.svg" alt="" className=' w-129px h-46px  rounded-none ' />
 
-<span className="min-w-120px cursor-pointer hover:text-blue-200  rtl text-xs text-gray font-600 text-14px">إنشاء المحتوى</span>
+<span className="  text-gray-400  cursor-pointer  hover:text-blue-950  ">إنشاء المحتوى</span>
 </div>
 
     
 
-<div className=' w-[40%] border p-1   rounded-xl'>
+<div className=' flex  items-center w-[40%] border p-1   rounded-xl'>
+<CiSearch className='   text-lg  text-gray-500' />
                 <input className='  outline-none rounded-sm    w-[90%] ' type=' text' placeholder=' ماذا تريد ان تتعلم اليوم ' /></div>
 
 <div>
-<button>تسجيل الدخول</button>
+<button className='  bg-green-500 p-1  border   opacity-75   px-7   text-white  ' >تسجيل الدخول</button>
 </div>
 
 
