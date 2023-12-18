496434cc969eaa9ea9ee39dd7b69d63319320b02
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2023-12-18 13:34:29 +0330

    install  firebase

---
+++ src/firbase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCDd-oXXepybhJY0OIkzV5CdfILQHIektM",
+  authDomain: "cursers2.firebaseapp.com",
+  projectId: "cursers2",
+  storageBucket: "cursers2.appspot.com",
+  messagingSenderId: "140685053606",
+  appId: "1:140685053606:web:b8b6f7bb80b8d79a61ae67"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
