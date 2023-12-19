62588e09958625cf4a3e2498ac38de68d294b762
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2023-12-19 10:40:39 +0330

    Singn out

--- src/assets/pages/Navbar2.jsx
+++ src/assets/pages/Navbar2.jsx
@@ -3,6 +3,7 @@
 import { auth } from '../../firbase/config';
 import { useAuthState } from 'react-firebase-hooks/auth';
 
+import { signOut } from "firebase/auth";
 
 const Navbar2 = () => {
   const [user, loading, error] = useAuthState(auth);
@@ -13,19 +14,31 @@ const Navbar2 = () => {
 <div className='flex  px-40  gap-8'>
 
 {user&& <h1>doneeeeeeee</h1>}
- <button className=' bg-white border  hover:bg-blue-200   rounded-xl  px-14'>
+{!user && <button className=' bg-white border  hover:bg-blue-200   rounded-xl  px-14'>
 
-<p className="  ">الک3333ل</p>
-</button> 
+<p className="  ">singh in</p>
+</button> }
 
 <button className='   bg-white border  hover:bg-blue-200   rounded-xl   px-7 flex justify-center items-center '>
 <img className='  w-7 ' src='src\assets\pages\imag\online-learning.png' />
-<p className="">دوره</p>
 </button>
-<button>
+{!user &&<button>
 
-<p className="">الکل</p>
-</button>
+<p className="">singh up</p>
+</button>}
+
+{user &&<button onClick={()=>{
+
+signOut(auth).then(() => {
+  // Sign-out successful.
+}).catch((error) => {
+  // An error happened.
+});
+
+}}>
+
+<p  className="">singh out</p>
+</button>}
 <button>
 
 <p className="">الکل</p>
