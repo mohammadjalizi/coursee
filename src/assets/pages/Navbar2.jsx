b7c5dbe845135e26318f165981980e92948ac771
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2023-12-19 09:51:59 +0330

    install  hook react

--- src/assets/pages/Navbar2.jsx
+++ src/assets/pages/Navbar2.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 
+import { auth } from '../../firbase/config';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+
 const Navbar2 = () => {
+  const [user, loading, error] = useAuthState(auth);
   return (
     <div className=' my-10  flex justify-between '>
         
 <div className='flex  px-40  gap-8'>
-<button className=' bg-white border  hover:bg-blue-200   rounded-xl  px-14'>
 
-<p className="  ">الکل</p>
-</button>
+  {!user&& <button className=' bg-white border  hover:bg-blue-200   rounded-xl  px-14'>
+
+<p className="  ">الک3333ل</p>
+</button> }
+
 <button className='   bg-white border  hover:bg-blue-200   rounded-xl   px-7 flex justify-center items-center '>
 <img className='  w-7 ' src='src\assets\pages\imag\online-learning.png' />
 <p className="">دوره</p>
