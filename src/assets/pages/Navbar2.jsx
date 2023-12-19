1c4807c9b691ddfeae173a2bc987a00c274634ec
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2023-12-19 10:31:40 +0330

    Add firbae hook

--- src/assets/pages/Navbar2.jsx
+++ src/assets/pages/Navbar2.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 
 import { auth } from '../../firbase/config';
 import { useAuthState } from 'react-firebase-hooks/auth';
@@ -6,15 +6,17 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 
 const Navbar2 = () => {
   const [user, loading, error] = useAuthState(auth);
+
   return (
     <div className=' my-10  flex justify-between '>
         
 <div className='flex  px-40  gap-8'>
 
-  {!user&& <button className=' bg-white border  hover:bg-blue-200   rounded-xl  px-14'>
+{user&& <h1>doneeeeeeee</h1>}
+ <button className=' bg-white border  hover:bg-blue-200   rounded-xl  px-14'>
 
 <p className="  ">الک3333ل</p>
-</button> }
+</button> 
 
 <button className='   bg-white border  hover:bg-blue-200   rounded-xl   px-7 flex justify-center items-center '>
 <img className='  w-7 ' src='src\assets\pages\imag\online-learning.png' />
