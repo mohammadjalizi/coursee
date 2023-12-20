573fc6bd21b821257fd06285e7a6979358faf961
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2023-12-20 12:46:06 +0330

    add  Frome

--- src/assets/pages/Frome1.jsx
+++ src/assets/pages/Frome1.jsx
@@ -6,6 +6,7 @@ const Frome1 = () => {
     const[email,Setemail]=useState("")
     const[password,Setpassword]=useState("")
     const[hasEror,SetEror]=useState(false)
+    const[hasfirbaseroro,Setfirbaseroro]=useState("")
   return (
     <div>
         
@@ -38,13 +39,44 @@ createUserWithEmailAndPassword(auth, email, password)
     const errorCode = error.code;
     const errorMessage = error.message;
     console.log(errorMessage)
+
     SetEror(true)
     // ..
+    switch (errorCode) {
+
+      case "auth/invalid-email":
+        setfirebaseError("Wrong Email")
+        break;
+
+
+      case "auth/user-not-found":
+        setfirebaseError("Wrong Email")
+        break;
+
+
+      case "auth/wrong-password":
+        setfirebaseError("Wrong Password")
+        break;
+
+
+      case "auth/too-many-requests":
+        setfirebaseError("Too many requests, please try aganin later")
+        break;
+
+
+      default:
+        setfirebaseError("Please check your email & password")
+        break;
+
+    }
+
+
+
   });
 
 }}>  singh in </button>
 
-{hasEror && <h1>eroooor</h1>}
+{hasEror && <h1>{hasfirbaseroro}</h1>}
 </form>
 
 
